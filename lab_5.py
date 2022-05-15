1246d577c8798b7682522bd3e41b7893ac1c1e3a
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-15 14:53:01 -0700

    finish code

--- lab_5.py
+++ lab_5.py
@@ -39,3 +39,21 @@ create dictionary():
 
 main()
 """
+
+def main():
+    sentence = input("Enter a sentence: ")
+    dictionary = create_dictionary("textese.txt")
+    translate(sentence, dictionary)
+
+def create_dictionary(txt_file):
+    infile = open(txt_file, "r")
+    words = [word.rstrip() for word in infile]
+    infile.close()
+    return dict([word.split(",") for word in words])
+
+def translate(sentence, dictionary):
+    words = sentence.spit()
+    for word in words:
+        print(dictionary.get(word, word), " ", end="") 
+
+main()
